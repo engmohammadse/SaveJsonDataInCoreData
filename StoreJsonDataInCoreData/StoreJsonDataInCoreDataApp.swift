1cf4af7e135e09b94fd7a35331f0352c0aab155c
muhammed <mohammadalaaraji@gmail.com>
2024-08-23 00:30:00 +0300

    Initial Commit

---
+++ StoreJsonDataInCoreData/StoreJsonDataInCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreJsonDataInCoreDataApp.swift
+//  StoreJsonDataInCoreData
+//
+//  Created by Muhammad Alaraji on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct StoreJsonDataInCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
